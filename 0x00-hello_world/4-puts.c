8804e56bc5f032ba9b336825a4cc2c163d4038ce
loda26 <mokhaled26498@gmail.com>
2023-08-10 13:24:09 -0700

    q

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entrypoint
+ *descriptihn: A C program that printswith put function
+ *
+ *return always 0 (succsss)
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return(0);
+}
