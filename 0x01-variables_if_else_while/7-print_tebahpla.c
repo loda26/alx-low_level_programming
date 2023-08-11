bfefa8a0efb9ffc08026c84e98207e72154d462b
loda26 <mokhaled26498@gmail.com>
2023-08-11 08:50:08 -0700

    q

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i = 'z';
+
+	while (i <= 'a')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar("\n");
+
+	return (0);
+}
