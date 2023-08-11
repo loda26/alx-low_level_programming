151da452d7ef5e49863d7c4ac4df5f7b7301f94b
loda26 <mokhaled26498@gmail.com>
2023-08-11 09:09:48 -0700

    q

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print from 0 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + 48)
+
+		if (I != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
