36ac99880bf10076daac2b2707f59bc7c04a8af9
loda26 <mokhaled26498@gmail.com>
2023-08-11 08:22:25 -0700

    q

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print numbers of base 10 starting from 0.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	return (0);
+}
