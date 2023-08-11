9385df37c1d64d9a321dacf5c8a326fa1e8d7d49
loda26 <mokhaled26498@gmail.com>
2023-08-10 18:19:51 -0700

    q

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - entry point
- * Description: A C program that prints with put function
  *
- * Return Always 0 (Succsss)
+ * Description: A C program that prints with puts function
+ *
+ * Return: Always 0 (Succsss)
 */
 
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
