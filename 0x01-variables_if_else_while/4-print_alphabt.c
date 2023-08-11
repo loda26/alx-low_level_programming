9525d29cb46abcccb5f35aa751586c7fc51eb061
loda26 <mokhaled26498@gmail.com>
2023-08-11 08:44:36 -0700

    q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -24,6 +24,6 @@ int main(void)
 			x++;
 		}
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -17,6 +17,6 @@ int main(void)
 		printf("%i", i);
 		i++;
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
