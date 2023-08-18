054083581013d13fd49887b0cce66977d944b0e7
loda26 <mokhaled26498@gmail.com>
2023-08-18 14:09:06 -0700

    q

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - a very long description
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		int three = i % 3;
+		int five = i % 5;
+
+		if (three == 0 && five == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (three == 0)
+		{
+			printf("Fizz");
+		}
+		else if (five == 0)
+		{
+		printf("Buzz");
+		}
+		else
+		{
+		printf("%i", i);
+		}
+		printf(" ");
+	}
+
+	return (0);
+}
