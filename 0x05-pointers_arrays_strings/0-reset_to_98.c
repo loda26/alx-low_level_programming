d80dd56e46a62d3903f46736af535dd00039e68c
loda26 <mokhaled26498@gmail.com>
2023-08-21 13:23:59 -0700

    q

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointeras parameter
+ *		and updates the value it points to to 98
+ *
+ * @n: int para
+ *
+ * Return: none
+*/
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c : The character to print
+ *
+ * Return: On success 1.
+ *         On error. -1 in returned. and reeno is set appropriately
+ */
+
+int _potchar(char c)
+{
+	return (write(1, &c, 1));
+}
