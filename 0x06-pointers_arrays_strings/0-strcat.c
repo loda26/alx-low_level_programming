45b0a05e297cb7a324a1440cbbdbe302424b9a46
loda26 <mokhaled26498@gmail.com>
2023-08-23 18:23:47 -0700

    q

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int c1 = 0, c2;
+	int c1, c2;
+
+	c1 = 0;
 
 	while (dest[c1])
 		c1++;
-	for (c2 = 0; src[c2]; c++)
+	for (c2 = 0; src[c2] != '\0'; c2++)
 		dest[c1++] = src[c2];
 	return (dest);
 }
