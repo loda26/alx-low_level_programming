2156a7d38c7bf528071b8ad2ef0a12f4ee9a57fa
loda26 <mokhaled26498@gmail.com>
2023-09-01 14:52:41 -0700

    q

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: noof command arg
+ * @argv: pointer
+ *
+ * Return: 1 on success
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
