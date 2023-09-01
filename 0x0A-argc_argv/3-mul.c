b329d369e34a944239e0dbcff66115d1ed77aa0f
loda26 <mokhaled26498@gmail.com>
2023-09-01 15:10:36 -0700

    q

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: int
+ * @argv: list
+ *
+ * Return: 0 on success, 1 on error
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1] * atoi(argv[2]));
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
