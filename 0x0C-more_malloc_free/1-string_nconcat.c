f79b7355caba8e043b88cfc4935b6546d6a78734
loda26 <mokhaled26498@gmail.com>
2023-09-07 14:42:30 -0700

    q

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * string_nconcat -  concatenates two strings
+ * @s1: input
+ * @s2: input
+ * @n: input
+ * Return: pointer
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int *str, lens1, lens2, i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (lens1 = 0; s1[lens1] != '\0'; lens1++)
+		;
+	for (lens2 = 0; s2[lens2] != '\0'; lens2++)
+		;
+
+	str = malloc(lens1 + n + 1);
+
+	for (i = 0; i < lens1; i++)
+		str[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
