a4d0eadfa83ab7605207347b4e457bd15d48a533
loda26 <mokhaled26498@gmail.com>
2023-09-08 02:41:29 -0700

    q

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,6 +20,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	if (pyr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
 	if (new_size > old_size)
 	{
 		p = malloc(new_size);
