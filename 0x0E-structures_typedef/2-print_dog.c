a6e728035189a6c0383fa79b343240def311f139
loda26 <mokhaled26498@gmail.com>
2023-09-11 08:00:39 -0700

    q

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - description
+ * @d: input
+ * Return: void
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
