558ba9b236062373cffd2aa1e77fcf8cd8932b79
loda26 <mokhaled26498@gmail.com>
2023-09-13 06:55:49 -0700

    q

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add
+ * @a: input
+ * @b: input
+ * Return: sum
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub
+ * @a: input
+ * @b: input
+ * Return: sub
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul
+ * @a: input
+ * @b: input
+ * Return: mul
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div
+ * @a: input
+ * @b: input
+ * Return: div
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod
+ * @a: input
+ * @b: input
+ * Return: mod
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
