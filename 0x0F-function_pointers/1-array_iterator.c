e35cf47fbd85cd70a4d1b0b20cb5a7a50cb2099a
loda26 <mokhaled26498@gmail.com>
2023-09-13 05:55:23 -0700

    q

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,8 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int *x = array + size - 1;
 
 	if (array && size && action)
-	{
 		while (array <= x)
 			action(*array++);
-	}
 }
