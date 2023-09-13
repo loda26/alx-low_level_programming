891fa5d907c6962b1f66cb50a37f456767cad2ff
loda26 <mokhaled26498@gmail.com>
2023-09-13 06:07:10 -0700

    q

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointer.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: input
+ * @size: input
+ * @cmp: input
+ * Return: 0, -1 or 1
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}
