1b3f2f4bcfc3e24576a014fbbc235d7e62fdabed
loda26 <mokhaled26498@gmail.com>
2023-09-14 03:24:31 -0700

    q

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 	va_start(list, n);
 	while (i--)
-		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)",
+		printf("%s%s", (str = va_arg(list, char *)) ? str : "(nil)",
 				i ? (separator ? separator : "") : "\n");
 	va_end(list);
 }
