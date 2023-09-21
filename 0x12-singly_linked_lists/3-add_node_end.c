68bb3133d6ce98a80c869272d658d102edd7bff4
loda26 <mokhaled26498@gmail.com>
2023-09-21 04:47:44 -0700

    q

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	if (str)
 	{
-		new_node->str = strdep(str);
+		new_node->str = strdup(str);
 		if (!new_node->str)
 		{
 			free(new_node);
