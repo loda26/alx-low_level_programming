4c0e463ef1e84c2d1585835e40485302508ac8ab
loda26 <mokhaled26498@gmail.com>
2023-09-21 04:26:42 -0700

    q

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,10 +2,18 @@
 
 /**
  * list_len - returns the number of elements in a linked list_t list
- * @h:
- * Return:
+ * @h: pointer to the first node
+ * Return: size of list
 */
 
 size_t list_len(const list_t *h)
 {
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
