c8f8d94fea7c9e860a05fbf7d72324dc38d1c62c
loda26 <mokhaled26498@gmail.com>
2023-09-25 04:01:06 -0700

    q

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: input head
+ * @n: index
+ * Return: the address of the new element, or NULL if it failed
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!head || !new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head)
+		new_node->next = head;
+	*head = new_node;
+	return (new_node);
+}
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: head input
+ * Return: void
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
