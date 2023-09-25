acd2fcbe470ee83dc3947d261d053cc101a16a11
loda26 <mokhaled26498@gmail.com>
2023-09-25 13:03:56 -0700

    q

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	if (*head)
-		new_node->next = head;
+		new_node->next = *head;
 	*head = new_node;
 	return (new_node);
 }
