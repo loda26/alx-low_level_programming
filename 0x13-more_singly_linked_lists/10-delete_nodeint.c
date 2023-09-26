d8ff827a4ae4f16138a9a3ef52d20133e15239b6
loda26 <mokhaled26498@gmail.com>
2023-09-26 13:36:32 -0700

    q

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (!index)
 	{
 		node = *head;
-		*head = (*head)->head;
+		*head = (*head)->next;
 		free(node);
 		return (1);
 	}
