2efcf9585fd42f93b34514c427aed25cea3d6088
loda26 <mokhaled26498@gmail.com>
2023-09-29 01:44:58 -0700

    q

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/***/
+
+void print_binary(unsigned long int n);
+{
+	int bit = sizeof(n) * 8, print = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			print++;
+		}
+		else if (print)
+			_putchar('0');
+	}
+	if (!print)
+		_putchar('0');
+}
