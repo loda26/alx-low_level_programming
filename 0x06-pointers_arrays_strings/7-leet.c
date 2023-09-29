76b53a658c102e283981e47c21db74dc90cc4e02
loda26 <mokhaled26498@gmail.com>
2023-09-29 09:48:36 -0700

    q

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#inlcude "main.h"
+
+/**
+ * leet - desc
+ * @c: input
+ * Return: string
+*/
+
+char *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0 ; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+				*c = 48 + value[i];
+		}
+		c++;
+	}
+	return (cp);
+}
