7cdd56ed1a0e944f2fcfbb7f993b8a4ed241b25c
loda26 <mokhaled26498@gmail.com>
2023-09-29 09:30:49 -0700

    q

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: input
+ * Return: string
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != NULL; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	}
+	return (str);
+}
