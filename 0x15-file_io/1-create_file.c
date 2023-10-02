ac2aa3c1082286e337679fc01bdb0419415151ed
loda26 <mokhaled26498@gmail.com>
2023-10-02 05:20:50 -0700

    q

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,11 +26,12 @@ int _strlen(char *s)
 
 int create_file(const char *filename, char *text_content);
 {
-	int fptr, bits, len = _strlen(text_content);
+	int fptr;
+	ssize_t bits = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	fptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fptr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	bits = write(fptr, text_content, len);
 	if (fptr == -1 || bits == -1)
 		return (-1);
