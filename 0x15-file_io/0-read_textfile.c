69d2e59295326f4e915cd97e4cc6d9cce5817b0b
loda26 <mokhaled26498@gmail.com>
2023-10-02 04:34:49 -0700

    task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file and prints it to the standard output
+ * @filename: input
+ * @letters: input
+ * Return: actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fptr, output, bits;
+	char *buff;
+
+	if (!filename || !letters)
+		return (0);
+	fptr = open(filename, O_RONLY);
+	if (fptr == -1)
+		return (0);
+	buff - malloc(sizeof(char) * letters);
+	bits = read(fptr, buff, letters);
+	output = write(STDOUT_FILENO, buff, bits);
+	close(fptr);
+	return (output);
+}
