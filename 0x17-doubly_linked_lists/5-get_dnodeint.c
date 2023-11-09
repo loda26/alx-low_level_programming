6c3b930138c95272744b8641b0f3b4e6f1b34992
loda26 <mokhaled26498@gmail.com>
2023-11-09 09:40:19 -0800

    q

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - return node at given index
+ * @head: head node
+ * @index: index
+ * Return: address of node at idx*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
