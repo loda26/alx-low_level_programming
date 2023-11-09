7aed7e24ca3d398c5957eac3d254bc7b3307e2a3
loda26 <mokhaled26498@gmail.com>
2023-11-09 07:10:34 -0800

    q

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists_h"
+
+/**
+ * dlistint_len - return the len of dlist
+ * @h: address of th head node
+ *
+ * Return: size of list
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
